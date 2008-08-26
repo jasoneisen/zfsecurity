5fe3b73c310a2216354765b5d757f5ce36d317f6
jasoneisen <jasoneisen@07891380-ad54-0410-b6ab-99bdfe4db8d5>
2008-08-26 13:18:12 +0000

    Merging plugin addition to alpha

--- modules/security/library/Security/Controller/Plugin/Loader.php
+++ modules/security/library/Security/Controller/Plugin/Loader.php
@@ -22,4 +22,24 @@ class Security_Controller_Plugin_Loader extends Zend_Controller_Plugin_Abstract
 		    $router->addConfig($routes);
 		}
 	}
+	
+	public function dispatchLoopStartup(Zend_Controller_Request_Abstract $request)
+	{
+	    if ($request->getModuleName() == 'security') {
+	        
+	        $front = Zend_Controller_Front::getInstance();
+	        
+	        if ($request->getControllerName() == $front->getDefaultControllerName() && $request->getControllerName() != 'index') {
+	            
+			    $request->setControllerName('index');
+			    $request->setActionName('index');
+			    $request->setDispatched(false);
+	            
+	        } elseif ($request->getActionName() == $front->getDefaultAction() && $request->getActionName() != 'index') {
+	            
+			    $request->setActionName('index');
+			    $request->setDispatched(false);
+            }
+        }
+	}
 }
